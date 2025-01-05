d68fe71f2fd3688a57ae9497ff36916426cc9c3f
Muhammad Ali <123479909+spaqootech@users.noreply.github.com>
2025-01-05 19:48:51 +0500

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+'use dom';
+
+// Remember to import the global.css file in each DOM component.
+import '../global.css';
+
+export default function Page() {
+  return (
+    <div className="bg-slate-100 rounded-xl">
+      <p className="text-6xl font-medium">Welcome to Tailwind</p>
+    </div>
+  );
+}
